683a9b81234f07e98c44869fe7c00ef2cf0481c8
Mike Joyce <mikejoyce19@gmail.com>
2015-03-22 12:19:25 -0500

    Initial commit

---
+++ Bookmarky.CoreSite/App_Start/BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Bookmarky.CoreSite.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundle/bowerjs")
+                //bower:js
+                
+                
+                
+                
+                .Include("~/ThirdParty/bower_components/modernizr/modernizr.js")
+                .Include("~/ThirdParty/bower_components/jquery/dist/jquery.js")
+                .Include("~/ThirdParty/bower_components/angular/angular.js")
+                .Include("~/ThirdParty/bower_components/bootstrap/dist/js/bootstrap.js")
+                .Include("~/ThirdParty/bower_components/moment/moment.js")
+                .Include("~/ThirdParty/bower_components/toastr/toastr.js")
+                //endbower
+                );
+
+            bundles.Add(new StyleBundle("~/bundle/bowercss")
+                //bower:css
+                
+                
+                
+                
+                .Include("~/ThirdParty/bower_components/bootstrap/dist/css/bootstrap.css")
+                .Include("~/ThirdParty/bower_components/toastr/toastr.css")
+                //endbower
+                );
+
+            bundles.Add(new StyleBundle("~/bundle/css")
+                .Include("~/Content/Site.css")
+                );
+        }
+    }
+}
---
+++ Bookmarky.CoreSite/Gruntfile.js
@@ -0,0 +1,32 @@
+﻿module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        wiredep: {
+            target: {
+                src: [
+                'App_Start/BundleConfig.cs'
+                ],
+                ignorePath: '..',
+                fileTypes: {
+                    cs: {
+                        block: /(([ \t]*)\/\/\s*bower:*(\S*)\s*)(\n|\r|.)*?(\/\/\s*endbower\s*)/gi,
+                        detect: {
+                            js: /<script.*src=['"](.+)['"]>/gi,
+                            css: /<link.*href=['"](.+)['"]/gi
+                        },
+                        replace: {
+                            js: '.Include("~{{filePath}}")',
+                            css: '.Include("~{{filePath}}")',
+                        },
+                    }
+                },
+                dependencies: true,
+                devDependencies: false,
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    grunt.registerTask('default', ['wiredep']);
+}
