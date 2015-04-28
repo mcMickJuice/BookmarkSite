266f0da80c1b31f4b7b2213a0b80e9c71c5623a6
Mike Joyce <mikejoyce19@gmail.com>
2015-04-27 23:09:45 -0500

    <Removed VS Solution>

--- Bookmarky.CoreSite/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bookmarky.CoreSite.MvcApplication" Language="C#" %>
--- Bookmarky.CoreSite/Scripts/app/bookmarkService.js
+++
@@ -1,9 +0,0 @@
-﻿var app = angular.module("bookmarkyApp");
-
-(function() {
-   function BookmarkService($resource) {
-       alert("hello");
-   }
-
-    app.factory("bookmarkService", ["$resource", BookmarkService]);
-}());
