3854d2ee9cf51822a11c1fd91e454af7fc0ee819
mcmickjuice <mikejoyce19@gmail.com>
2015-06-02 11:59:35 -0500

    Created ResourceType directive, added to Bookmark detail view. Added resource type images

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,32 +13,34 @@ angular
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    // 'ngRoute',
     'ngSanitize',
     'ngTouch',
     'toastrjs',
     'ui.bootstrap',
     'ui.router'
   ])
-  // .config(function ($routeProvider) {
-  //   $routeProvider
-  //     .when('/', {
-  //       templateUrl: 'views/main.html',
-  //       controller: 'mainCtrl'
-  //     })
-  //     .when('/bookmarks/create',{
-  //       templateUrl: 'views/bookmarkCreate.html',
-  //       controller: 'bookmarkCtrl'
-  //     })
-  //     .when('/bookmarks',{
-  //       templateUrl: 'views/bookmarkSearch.html',
-  //       controller: 'bookmarkSearchCtrl'
-  //     })
-  //     .when('/bookmarks/:id',{
-  //       templateUrl: 'views/bookmarkDetail.html',
-  //       controller: 'bookmarkCtrl'
-  //     });
-  // });
+  .constant('RESOURCE_DESCRIPTION',{ //maybe should be a json file
+    1:{
+      name: 'Article',
+      imageUrl: 'article.png',
+      description: 'Bookmark is an article or blogpost'
+    },
+    2:{
+      name: 'Tutorial',
+      imageUrl: 'tutorial.png',
+      description: 'Bookmark is a post about how to do a specific thing'
+    },
+    3:{
+      name: 'Video',
+      imageUrl: 'video.png',
+      description: 'Bookmark is a video or slideshow'
+    },
+    4:{
+      name: 'Reference',
+      imageUrl: 'reference.jpg',
+      description: "Bookmark is a site that's used as a reference (e.g. MDN)"
+    },
+  })
   .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
---
+++ app/scripts/directives/resourceType.js
@@ -0,0 +1,34 @@
+(function() {
+	"use strict";
+	// function ResourceTypeCtrl($scope, RESOURCE_DESCRIPTION) {
+	// 	var vm = this;
+	// 	debugger;
+	// 	vm.resourceInfo = RESOURCE_DESCRIPTION[$scope.resourceId];
+	// }
+
+	function ResourceType(RESOURCE_DESCRIPTION) {
+		var link = function(scope, element, attr) {
+			attr.$observe('resourceId', function(value) {
+				if (value) {
+					scope.resourceInfo = RESOURCE_DESCRIPTION[value];
+					scope.imageSrc = '/images/' + scope.resourceInfo.imageUrl;
+				}
+			});
+
+			scope.$watch('resourceInfo');
+		}
+
+		return {
+			restrict: 'E',
+			templateUrl: 'views/directives/resourceType.html',
+			scope: {
+				resourceId: '@'
+			},
+			link: link
+		}
+	}
+
+	angular.module('bookmarkysiteApp')
+		.directive('bmResourceType', ['RESOURCE_DESCRIPTION', ResourceType])
+		//.controller('resourceTypeCtrl', ['$scope', 'RESOURCE_DESCRIPTION', ResourceTypeCtrl])
+}())
