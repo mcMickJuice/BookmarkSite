c8a9764a5b539c981b52263804273936e5a268a3
Mike Joyce <mikejoyce19@gmail.com>
2015-10-16 22:20:12 -0500

    Removed unused scripts

--- src/app/components/tagSearch/tagSearch.component.js
+++ src/app/components/tagSearch/tagSearch.component.js
@@ -1,27 +1,3 @@
-// (function(angular){
-// 	'use strict';
-
-// 	function TagSearch(){
-// 			return {
-// 			templateUrl: 'views/tagSearch.html',
-// 			scope: {
-// 				allTags: '=allTags',
-// 				selectedTags: '=selectedTags',
-// 				createTag: '&createTag',
-// 				canAdd: '=canAdd'
-// 			},
-// 			link: function(scope){
-// 				scope.addTag = function(tag){
-// 					scope.selectedTags.push(tag);
-// 				};
-// 			}
-// 		};
-// 	}
-
-// 	angular.module('bookmarkysiteApp')
-// 		.directive('tagSearch',TagSearch);
-// }(window.angular));
-
 import template from './tagSearch.tmpl.html!text'
 
 function TagSearch() {
